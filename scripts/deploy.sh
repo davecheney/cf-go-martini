95813e68c1099ffe27bdf0f3047775141b2ef886
Dave Cheney <dave@cheney.net>
2016-08-05 13:45:26 +1000

    fix typo (#1)
    * fix typo
    
    * removed branch filter
    
    * :staging must contain a branch or tag ¯\_(ツ)_/¯
    
    * no, the other kind of regex
    
    * grr typo, (凸ಠ益ಠ)凸
    
    * inverted my logic was
    
    * case, reduce your paranoia setting
    
    * try a differnt way to get the PR details
    
    * ffffuuuu
    
    * scripting intensifies

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 set -eu # please dont use -x as this will print secrets in the build log
 
+
 # these env vars can be overridden by circle
 CF_API=${CF_API:-https://api.system.staging.digital.gov.au}
 CF_ORG=${CF_ORG:-dto}
@@ -9,7 +10,7 @@ CF_SPACE=${CF_SPACE:-dfc-test}
 
 # step 1. login to the correct org and space with cf
 cf api ${CF_API}
-cf auth ${CF_STAGING_USERxi} {$CF_STAGING_PASSWORD}
+cf auth ${CF_STAGING_USER} ${CF_STAGING_PASSWORD}
 cf target -o ${CF_ORG} -s ${CF_SPACE}
 
 # step 2. create db service and app for this branch
@@ -19,12 +20,12 @@ cf push ${CF_APP_NAME} --no-start
 cf create-service dto-shared-pgsql shared-psql ${CF_SERVICE_NAME}
 cf bind-service ${CF_APP_NAME} ${CF_SERVICE_NAME}
 
-# step 3. push branch details into the deployed app's env
-if [ -z ${CIRCLE_PR_REPONAME} ] ; then
-	cf set-env ${CF_APP_NANE} CIRCLE_PR_REPONAME ${CIRCLE_PR_REPONAME}
-fi
-if [ -z ${CIRCLE_PR_NUMBER} ] ; then
-	cf set-env ${CF_APP_NANE} CIRCLE_PR_NUMBER ${CIRCLE_PR_NUMBER}
+# step 3. is this a PR? if so, push the PR details into the cf app env
+
+if [ -z "${CI_PULL_REQUEST:-}" ] ; then
+	echo "not a pull request"
+else
+	cf set-env ${CF_APP_NAME} CI_PULL_REQUEST ${CI_PULL_REQUEST}
 fi
 
 # step 4. fire!
